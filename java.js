75f31f0af183d1f31027c2e9d1699e240179fa2d
lequybkaa <lequy0305@gmail.com>
2013-08-14 21:16:05 +0700

    first upload

---
+++ java.js
@@ -0,0 +1,36 @@
+﻿function search(fn){
+	var keyword = fn.txtKeyword.value;
+	if(keyword==""||keyword=="Search keywords"){
+		window.alert('Nhập từ khóa tìm kiếm.');
+		fn.txtKeyword.focus();
+		fn.txtKeyword.value="";
+	}
+}
+
+function p(fn){
+	fn.txtKeyword.value = "Search keywords";
+	fn.txtKeyword.style.color="#aafff";
+}
+
+function checkKeyword(fn, iEnter){
+	if(iEnter){
+		var keyword = fn.txtKeyword.value;
+		if(keyword=="Search keywords"){
+			fn.txtKeyword.value="";//KICK VAO O S THI MAT CHU SEARCH KEYWORDS
+		}
+	}else{
+		var keyword = fn.txtKeyword.value;
+		if(keyword==""){
+			fn.txtKeyword.value = "Search keywords";//KO CO KI TU THI KICJ RA NGOAI SE HIEN LEN EARCH KEYWORD
+		}
+	}
+}
+
+function format(fn){
+	var keyword = fn.txtKeyword.value;
+	if(keyword!="Search keywords"){
+		fn.txtKeyword.style.color=black;//VIET KI TU KHAC MA KICH RA NGOAI SE DOI MAU
+	}else{
+		fn.txtKeyword.style.color=black;//CON LAI LA MAU KHAC
+	}
+}
